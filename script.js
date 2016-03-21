ec4c3556166cc6625151f58fe8f64a96e81791f4
erezalgazi <erezalgazi@gmail.com>
2016-03-21 19:56:28 +0200

    completed exercise

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+var beers = [];
+var sortOpposite = true;
+
 $('.post-beer').on('click', function (e) {
   e.preventDefault();
+  var name = $('#beer-name').val();
+  var cat = $('#beer-category').val();
+  var rate = $('#beer-rating').val();
+  addBeer(name,cat,rate);
+  updateBeer();
 });
+
+$('.sort-beer').on('click', function (e) {
+	e.preventDefault();
+	sortRating();
+});
+
+
+
+var addBeer = function	(name, category, rating) {
+	var beerObj = {
+		beerName: name,
+		beerCategory: category,
+		beerRating: rating
+	};
+	beers.push(beerObj);
+}
+
+var updateBeer = function () {
+	$('.beers').find('div').remove(); 
+	for (var i = 0; i < beers.length; i++) {
+		$('.beers').append('<div>' + "Beer name: " + beers[i].beerName + "; Beer type: " + beers[i].beerCategory + "; Beer rating: " + beers[i].beerRating  + '</div>');
+	}
+}
+
+var sortRating = function () {
+	if (sortOpposite){
+		beers.sort(function(a,b){
+		return b.beerRating - a.beerRating});
+		sortOpposite = false;
+	}
+	else {
+		beers.sort(function(a,b){
+		return a.beerRating - b.beerRating});
+		sortOpposite = true;
+	}
+	updateBeer();
+
+
+	// for (var i=0; i<beers.length; i++) {
+	// 	if (beers[i].beerRating < beers[i+1].beerRating) {
+	// 		var temp = beers[i];
+	// 		beers[i] = beers[i+1];
+	// 		beers[i+1] = temp;
+	// 	}
+	// }
+}
+
